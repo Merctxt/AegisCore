69875d2ec979c4060071b329768b0046841a02f6
Merctxt <giovannihettwer2@gmail.com>
2026-02-04 23:13:29 -0300

    feat: improvements variables

---
+++ AegisCoreWeb/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace AegisCoreWeb;
+
+public class AppSettings
+{
+    public string ApiUrl { get; set; } = "http://localhost:5050";
+    public string FrontendUrl { get; set; } = "http://localhost:5100";
+}
